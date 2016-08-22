7d38de4492c64e2f6406ef50f8ecc002c1b5e885
Kingdaro <kingdaro@gmail.com>
2016-08-22 15:18:00 -0400

    reversed camera position
    because it still makes a little more sense

--- game/camera.lua
+++ game/camera.lua
@@ -1,14 +1,14 @@
 local camera = {}
 
 function camera:init()
-  self.x = 30
-  self.y = 30
+  self.x = -30
+  self.y = -30
   self.zoom = 1
 end
 
 function camera:drag(dx, dy)
-  self.x = self.x + dx / self.zoom
-  self.y = self.y + dy / self.zoom
+  self.x = self.x - dx / self.zoom
+  self.y = self.y - dy / self.zoom
 end
 
 function camera:scrollZoom(amount, delta)
@@ -17,7 +17,7 @@ end
 
 function camera:getScenePosition(x, y)
   local sw, sh = love.graphics.getDimensions()
-  return (x - sw / 2) / self.zoom + sw / 2 - self.x, (y - sh / 2) / self.zoom + sh / 2 - self.y
+  return (x - sw / 2) / self.zoom + sw / 2 + self.x, (y - sh / 2) / self.zoom + sh / 2 + self.y
 end
 
 function camera:transform(draw)
@@ -26,7 +26,7 @@ function camera:transform(draw)
   love.graphics.translate(sw / 2, sh / 2)
   love.graphics.scale(self.zoom)
   love.graphics.translate(-sw / 2, -sh / 2)
-  love.graphics.translate(self.x, self.y)
+  love.graphics.translate(-self.x, -self.y)
   draw()
   love.graphics.pop()
 end
